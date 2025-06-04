fd83bca57d9f579a7b5c5da1239e84c917a26245
Maddog-ku <111139616+Maddog-ku@users.noreply.github.com>
2025-06-04 21:00:48 +0800

    Delete pytorch directory

--- pytorch/test.py
+++
@@ -1,8 +0,0 @@
-import torch
-
-print("PyTorch:", torch.__version__)
-print("CUDA runtime:", torch.version.cuda)
-print("CUDA available:", torch.cuda.is_available())
-print("GPU count:", torch.cuda.device_count())
-if torch.cuda.is_available():
-    print("GPU name:", torch.cuda.get_device_name(0))
