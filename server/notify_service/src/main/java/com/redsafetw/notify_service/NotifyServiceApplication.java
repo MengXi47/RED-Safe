9a3ce03fe408825eccd61f0fa7c8a9f70a2dbc4e
boen <chenboen931204@gmail.com>
2025-10-23 12:45:34 +0800

    test

--- server/notify_service/src/main/java/com/redsafetw/notify_service/NotifyServiceApplication.java
+++ server/notify_service/src/main/java/com/redsafetw/notify_service/NotifyServiceApplication.java
@@ -8,9 +8,7 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class NotifyServiceApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(NotifyServiceApplication.class, args);
-    }
-
+  public static void main(String[] args) {
+    SpringApplication.run(NotifyServiceApplication.class, args);
+  }
 }
